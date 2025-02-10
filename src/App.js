5db04ea2be1279aa3a61cd1c274608a579de24c4
dougiehawes <dougiehawes@hotmail.com>
2025-02-10 16:28:27 +0000

    added styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/layout/Header";
@@ -8,13 +9,19 @@ import SignUp from "./components/routes/public/SignUp";
 import "./display/styles/style.scss";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <div className="app">
-      <Header />
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      <Header darkmode={darkMode} onClick={() => setDarkMode(!darkMode)} />
+      <div
+        className={`body ${darkMode ? "body-dark-mode" : "body-light-mode"}`}
+      >
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
     </div>
   );
 };
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,34 @@
+import { FaMoon, FaPalette, FaSun } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+import "./style.scss";
+
+const Header = ({ darkmode, onClick }) => {
   return (
     <header className="header">
-      <Link to="/">
-        <h1 className="header-title">Klimpt-E</h1>
-      </Link>
+      <div className="header-title-container">
+        <Link className="header-title-link" to="/">
+          <FaPalette className="header-title-icon" size={42} />
+          <h1 className="header-title">KlimptE</h1>
+        </Link>
+      </div>
+      <div className="header-buttons-container">
+        <div className="header-darkmode-toggle-container">
+          {darkmode ? (
+            <FaSun
+              className="header-darkmode-toggle-icon"
+              onClick={onClick}
+              size={35}
+            />
+          ) : (
+            <FaMoon
+              className="header-darkmode-toggle-icon"
+              onClick={onClick}
+              size={35}
+            />
+          )}
+        </div>
+      </div>
     </header>
   );
 };
